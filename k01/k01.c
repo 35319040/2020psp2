b98a1749522ef22f49e84e988e1f6ee1d2b0a8f5
fujii erika <bb35319040@ms.nagasaki-u.ac.jp>
2020-04-22 21:33:15 +0900

    藤井英梨花です。k01の課題お願いします。

--- k01/k01.c
+++ k01/k01.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave);
+extern double var_online(double val,double ave,double square_ave);
+
+double N;
 
 int main(void)
 {
@@ -12,6 +14,13 @@ int main(void)
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    double ave=0;
+    double var=0;
+    double square_ave=0;
+    double u;
+    double val2=0;
+    double avee=0;
+    double square_avee;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -24,15 +33,34 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    N=0;
+   
+
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
+        N=N+1;
 
-    
+        avee=ave;
+        
+        ave=ave_online(val,ave);
+        square_avee=square_ave;
 
+        val2=val*val;
 
+        square_ave=ave_online(val2,square_ave);
+
+        var=var_online(val,avee,square_avee);
 
     }
+
+    u=N/(N-1)*var;
+
+    printf("ave=%lf\n",ave);
+    printf("var=%lf\n",var);
+    printf("expected ave=%lf\n",ave);
+    printf("expected var=%lf\n",u);
+
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
@@ -45,3 +73,22 @@ int main(void)
 
 }
 
+double ave_online(double val,double ave)
+{
+    double ave2;
+
+    ave2=(N-1)/N*ave+1/N*val;
+
+    return ave2;
+
+}
+
+double var_online(double val,double ave,double square_ave)
+{
+    double var;
+
+    var=((N-1)/N*square_ave)+(1/N*val*val)-(((N-1)/N*ave)+(1/N*val))*(((N-1)/N*ave)+(1/N*val));
+
+    return var;
+
+}
